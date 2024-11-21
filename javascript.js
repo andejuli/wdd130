75813b65a17939bed65eb5f2d2022a1acf6a699e
andejuli <techtrek@hotmail.com>
2024-11-21 09:26:19 -0700

    update week10

---
+++ javascript.js
@@ -0,0 +1,37 @@
+
+const PI = 3.14;
+let radius = 5;
+let area;
+
+area = radius * radius * PI;
+
+// console.log(area);
+
+// radius = 5;
+
+// area = radius * radius * pi;
+
+// console.log(area);
+
+ const one = 1;
+ const two = '2';
+// const three = 'e';
+
+// let result = one * two;
+
+// console.log (result);
+
+result = one + two;
+
+console.log(result);
+
+result = one + Number(two);
+
+console.log(result);
+
+
+
+
+
+
+
