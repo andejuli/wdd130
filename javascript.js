008ea769853970504f74f7294b33ed24cb6553ac
andejuli <techtrek@hotmail.com>
2024-12-10 13:15:27 -0700

    hoist

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let h1Content = document.querySelector('h1');
+h1Content.textContent = addIt(num1, num2);
+
 let num1 = 2;
 let num2 = 5;
 
@@ -5,8 +8,7 @@ function addIt(n1, n2) {
     return(n1 + n2)
 }
 
-let h1Content = document.querySelector('h1');
-h1Content.textContent = addIt(num1, num2);
+
 
 
 
