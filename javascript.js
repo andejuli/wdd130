bfad4cf112dd1cd8a9a105b8f63b489f3ac603cd
andejuli <techtrek@hotmail.com>
2024-12-10 13:18:49 -0700

    back

--- javascript.js
+++ javascript.js
@@ -1,14 +1,12 @@
+let h1Content = document.querySelector('h1');
 let num1 = 2;
 let num2 = 5;
-
-let h1Content = document.querySelector('h1');
-h1Content.textContent = addIt(num1, num2);
 
 function addIt(n1, n2) {
     return(n1 + n2)
 }
 
-
+h1Content.textContent = addIt(num1, num2);
 
 
 
